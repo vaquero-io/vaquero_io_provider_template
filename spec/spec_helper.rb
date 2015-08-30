4388eb06032d78b4e49e9f89e5cbaacb571df971
Nic Cheneweth <nic.cheneweth@thoughtworks.com>
2015-08-30 18:53:54 -0500

    coverage checking

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
-#
-# require 'codeclimate-test-reporter'
-# CodeClimate::TestReporter.start
+
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
